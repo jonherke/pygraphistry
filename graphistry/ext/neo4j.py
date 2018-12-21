9a13398f0b3fe10bf601ae6c4445eac283a7b4fc
Christopher Harris <xixonia@gmail.com>
2018-12-20 20:14:37 -0600

    .

--- graphistry/ext/neo4j.py
+++ graphistry/ext/neo4j.py
@@ -27,8 +27,8 @@ def _edge_table(relationships):
     attribute_names = _attributes_for_entities(relationships)
     return arrow.Table.from_arrays(
         [column for column in itertools.chain(
-            _intrinsic_edge_columns(relationships),
-            _columns_for_entity(relationships, attribute_names)
+            _intrinsic_edge_columns(relationships)
+            # _columns_for_entity(relationships, attribute_names)
         )]
     )
 
@@ -36,8 +36,8 @@ def _node_table(nodes):
     attribute_names = _attributes_for_entities(nodes)
     return arrow.Table.from_arrays(
         [column for column in itertools.chain(
-            _intrinsic_node_columns(nodes),
-            _columns_for_entity(nodes, attribute_names)
+            _intrinsic_node_columns(nodes)
+            # _columns_for_entity(nodes, attribute_names)
         )]
     )
 
@@ -46,11 +46,11 @@ def _attributes_for_entities(entities):
         key for entity in entities for key in entity.keys()
     )
 
-def _columns_for_entity(entities, entity_attributes):
-    for attribute in entity_attributes:
-        yield arrow.column(attribute, [
-            [entity[attribute] if attribute in entity else None for entity in entities]
-        ])
+# def _columns_for_entity(entities, entity_attributes):
+#     for attribute in entity_attributes:
+#         yield arrow.column(attribute, [
+#             [entity[attribute] if attribute in entity else None for entity in entities]
+#         ])
 
 def _intrinsic_edge_columns(relationships):
     yield arrow.column(EDGE_ID, [
@@ -65,40 +65,11 @@ def _intrinsic_edge_columns(relationships):
         [relationship.end_node.id for relationship in relationships]
     ])
 
-    yield arrow.column("__NEO4J_TYPE__", [
-        [relationship.type for relationship in relationships]
-    ])
+    # yield arrow.column("__neo4j_type__", [
+    #     [relationship.type for relationship in relationships]
+    # ])
 
 def _intrinsic_node_columns(nodes):
     yield arrow.column(NODE_ID, [
         [node.id for node in nodes]
     ])
-
-    yield arrow.column("__NEO4J_LABELS__", [
-        [list(node.labels) for node in nodes]
-    ])
-
-# def _node_fields(  # TODO(cwharris) make keys configurable
-#     node: neo4j.Node,
-#     node_id_column_name: str,
-#     neo4j_label_column_name: str
-# ):
-#     yield (node_id_column_name, node.id)
-#     yield (neo4j_label_column_name, node.labels)
-#     for item in node.items():
-#         yield item
-
-# def _relationship_fields( # TODO(cwharris) make keys configurable
-#     relationship: neo4j.Relationship,
-#     edge_id_column_name: str,
-#     edge_src_column_name: str,
-#     edge_dst_column_name: str,
-#     neo4j_type_column_name: str
-# ):
-#     yield (edge_id_column_name, relationship.id)
-#     yield (edge_src_column_name, relationship.start_node.id)
-#     yield (edge_dst_column_name, relationship.end_node.id)
-#     yield (neo4j_type_column_name, relationship.type)
-
-#     for item in relationship.items():
-#         yield item

816e972919be25db67264881b4885d1ff2a8e683
Christopher Harris <xixonia@gmail.com>
2018-12-21 14:30:17 -0600

    .

--- graphistry/ext/neo4j.py
+++ graphistry/ext/neo4j.py
@@ -27,8 +27,8 @@ def _edge_table(relationships):
     attribute_names = _attributes_for_entities(relationships)
     return arrow.Table.from_arrays(
         [column for column in itertools.chain(
-            _intrinsic_edge_columns(relationships)
-            # _columns_for_entity(relationships, attribute_names)
+            _intrinsic_edge_columns(relationships),
+            _columns_for_entity(relationships, attribute_names)
         )]
     )
 
@@ -36,8 +36,8 @@ def _node_table(nodes):
     attribute_names = _attributes_for_entities(nodes)
     return arrow.Table.from_arrays(
         [column for column in itertools.chain(
-            _intrinsic_node_columns(nodes)
-            # _columns_for_entity(nodes, attribute_names)
+            _intrinsic_node_columns(nodes),
+            _columns_for_entity(nodes, attribute_names)
         )]
     )
 
@@ -46,11 +46,11 @@ def _attributes_for_entities(entities):
         key for entity in entities for key in entity.keys()
     )
 
-# def _columns_for_entity(entities, entity_attributes):
-#     for attribute in entity_attributes:
-#         yield arrow.column(attribute, [
-#             [entity[attribute] if attribute in entity else None for entity in entities]
-#         ])
+def _columns_for_entity(entities, entity_attributes):
+    for attribute in entity_attributes:
+        yield arrow.column(attribute, [
+            [entity[attribute] if attribute in entity else None for entity in entities]
+        ])
 
 def _intrinsic_edge_columns(relationships):
     # TODO(cwharris): remove the string conversion once server can haandle non-ascending integers.
@@ -67,13 +67,17 @@ def _intrinsic_edge_columns(relationships):
         [str(relationship.end_node.id) for relationship in relationships]
     ])
 
-    # yield arrow.column("__neo4j_type__", [
-    #     [relationship.type for relationship in relationships]
-    # ])
+    yield arrow.column("__neo4j_type__", [
+        [relationship.type for relationship in relationships]
+    ])
 
 def _intrinsic_node_columns(nodes):
     # TODO(cwharris): remove the string conversion once server can haandle non-ascending integers.
     # currently, ids will be remapped as part of pre-plot rectification.
     yield arrow.column(NODE_ID, [
         [str(node.id) for node in nodes]
+    ])
+
+    yield arrow.column("__neo4j_label__", [
+        [list(node.labels) for node in nodes]
     ])
